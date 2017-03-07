2a4fe33006f46b4f036f1846874ef869243d5743
James Y. Rauhut <jamesrauhut@us.ibm.com>
2017-03-06 20:15:11 -0600

    Fetch API Data While Keeping Keys Secure

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 import compression from 'compression';
 import express from 'express';
+import fs from 'fs';
+import request from 'request';
 
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(compression());
 
-app.use(express.static('./build'));
+let keys;
+if (fs.existsSync('./keys.json')) {
+  keys = require('./keys.json');
+} else {
+  keys = JSON.parse(process.env.VCAP_SERVICES)['user-provided'][0].credentials;
+}
 
+app.get('/github', (req, res) => {
+  request({
+    url: `https://api.github.com/user/repos?affiliation=owner,collaborator&access_token=${keys.github}`,
+    headers: {
+      'user-agent': 'node.js',
+    },
+  }, (err, response, body) => {
+    if (!err && response.statusCode === 200) {
+      res.send(body);
+    }
+  });
+});
+
+app.use(express.static('./build'));
 app.listen(port, (err) => {
- if (err) {
-   console.log(err);
-   return;
- }
- console.log(`Server is live at http://localhost:${port}`);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Server is live at http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    repos: [],
+  }
+
+  componentDidMount() {
+    fetch('/github')
+    .then(response => response.json())
+    .then((data) => {
+      const repos = data.map((repo) =>
+        <p key={repo.id}>{repo.name}</p>
+      );
+
+      this.setState({ repos })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +30,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h3>App Creator's Repos:</h3>
+        {this.state.repos}
       </div>
     );
   }
